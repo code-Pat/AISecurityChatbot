bf3cb366514c17eff7932e1f27b2e10cb174e6d7
Donggeun Lee <patlee997@gmail.com>
2026-04-28 15:40:07 +0900

    Initial Commit

---
+++ AISecurityChatbot/AISecurityChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AISecurityChatbotApp.swift
+//  AISecurityChatbot
+//
+//  Created by Donggeun Lee on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct AISecurityChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
